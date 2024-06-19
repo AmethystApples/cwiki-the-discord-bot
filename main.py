f92160af9b16504ef3ce6359b375207422926e62
eugen <e.joo@wustl.edu>
2024-06-19 10:20:36 -0500

    debug

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import discord
 
 bot = discord.Client(intents=discord.Intents.default())
 
+STARTUP_CHANNEL_ID = 1252983015169196177
+
 @bot.event
 
 async def on_ready():
@@ -21,12 +23,23 @@ async def on_ready():
         
     print("SampleDiscordBot is in " + str(guild_count) + " guilds.")
 
+    #channel = bot.get_channel(STARTUP_CHANNEL_ID)
+    #if channel:
+    #   await channel.send("SampleDiscordBot has started and is now online!")
+
 @bot.event
 async def on_message(message):
 	# CHECKS IF THE MESSAGE THAT WAS SENT IS EQUAL TO "HELLO".
-	if message.content == "hello":
+    #if message.author == bot.user:
+    #    return
+
+    print("wow, i just got a message")
+    if message.content == "hello":
 		# SENDS BACK A MESSAGE TO THE CHANNEL.
-		await message.channel.send("hey dirtbag")
+        print("hi")
+        channel = bot.get_channel(STARTUP_CHANNEL_ID)
+        if channel:
+            await channel.send("SampleDiscordBot has started and is now online!")
 
 # EXECUTES THE BOT WITH THE SPECIFIED TOKEN. TOKEN HAS BEEN REMOVED AND USED JUST AS AN EXAMPLE.
 bot.run("MTI1Mjk4MTE2NjE4MTcxMTg3NA.GplJmK.CAPGvqFNy1OlfgSMtWznlTsd_HOov-VWRJVlyI")
